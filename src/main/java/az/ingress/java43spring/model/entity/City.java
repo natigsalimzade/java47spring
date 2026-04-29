67eb21373a6f7fc19d29fbc7926945b80dbe6522
Natiq Salimzade <nicoselim008@gmail.com>
2026-04-29 14:33:32 +0400

    test

--- src/main/java/az/ingress/java43spring/model/entity/City.java
+++ src/main/java/az/ingress/java43spring/model/entity/City.java
@@ -1,6 +1,5 @@
 package az.ingress.java43spring.model.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
